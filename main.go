03b1f4ce35bba4dfbf7503d97dc2e15875135eab
zerosign <r1nlx0@gmail.com>
2022-07-25 22:21:06 +0700

    chore: add a way to debug the I/O stuffs
    Signed-off-by: zerosign <r1nlx0@gmail.com>

--- main.go
+++ main.go
@@ -12,6 +12,10 @@ import (
 	"strings"
 )
 
+const (
+	gpgProgram = "gpg"
+)
+
 var (
 	files []string = []string{
 		".authinfo.gpg",
@@ -21,6 +25,12 @@ var (
 	}
 )
 
+type Remote struct {
+	Protocol string
+	Host     string
+	Username string
+}
+
 type Credential struct {
 	Host        string
 	Username    string
@@ -28,7 +38,49 @@ type Credential struct {
 	Password    string
 }
 
-func readLine(line string) (Credential, error) {
+func NextLineValue(scanner *bufio.Scanner, key string) (string, error) {
+	if !scanner.Scan() {
+		return "", fmt.Errorf("empty line")
+	}
+
+	value := scanner.Text()
+
+	parts := strings.Split(value, "=")
+
+	if len(parts) != 2 {
+		return "", fmt.Errorf("value for '%s' not exists", key)
+	}
+
+	return parts[1], nil
+}
+
+func readRemoteLine(scanner *bufio.Scanner) (Remote, error) {
+	protocol, err := NextLineValue(scanner, "protocol")
+
+	if err != nil {
+		return Remote{}, err
+	}
+
+	host, err := NextLineValue(scanner, "host")
+
+	if err != nil {
+		return Remote{}, err
+	}
+
+	username, err := NextLineValue(scanner, "username")
+
+	if err != nil {
+		return Remote{}, err
+	}
+
+	return Remote{
+		Protocol: protocol,
+		Host:     host,
+		Username: username,
+	}, nil
+}
+
+func readCredentialLine(line string) (Credential, error) {
 	parts := strings.Split(line, " ")
 
 	if len(parts) != 6 {
@@ -51,13 +103,25 @@ func readLine(line string) (Credential, error) {
 
 func main() {
 
-	stdinScanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+	homeDir, err := os.UserHomeDir()
 
-	for stdinScanner.Scan() {
-		fmt.Println(stdinScanner.Text())
+	if err != nil {
+		log.Fatal(err)
 	}
 
-	homeDir, err := os.UserHomeDir()
+	logFile, err := os.OpenFile(filepath.Join(homeDir, "authinfo.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+
+	stdinScanner := bufio.NewScanner(bufio.NewReader(os.Stdin))
+
+	remote, err := readRemoteLine(stdinScanner)
 
 	if err != nil {
 		log.Fatal(err)
@@ -66,31 +130,38 @@ func main() {
 	localHomeFs := os.DirFS(homeDir)
 
 	for _, file := range files {
+
 		if _, err := fs.Stat(localHomeFs, file); err == nil {
 
 			authInfoPath := filepath.Join(homeDir, file)
 
 			if filepath.Ext(authInfoPath) == ".gpg" {
 
-				fmt.Println("path: ", authInfoPath, filepath.Ext(authInfoPath))
+				log.Println("path: ", authInfoPath, filepath.Ext(authInfoPath))
 
-				cmd := exec.Command("gpg", "--decrypt", authInfoPath)
+				cmd := exec.Command(gpgProgram, "--decrypt", authInfoPath)
 				raw, err := cmd.Output()
 
 				if err != nil {
-					log.Fatal(err)
+					log.Fatal("error on executing ", cmd.String(), " ", err)
 				}
 
 				scanner := bufio.NewScanner(bytes.NewReader(raw))
 
 				// machine [host] login [username]^[application] password [password]\n
 				for scanner.Scan() {
-					credential, err := readLine(scanner.Text())
+					credential, err := readCredentialLine(scanner.Text())
+
 					if err != nil {
 						log.Fatal(err)
 					}
 
-					log.Println("credential: ", credential)
+					log.Println(credential.Host, remote.Host, credential.Username, remote.Username)
+
+					if credential.Host == remote.Host && credential.Username == remote.Username {
+						// username=you\npassword=easy\n\n
+						log.Printf("username=%s\npassword=%s\n\n", credential.Username, credential.Password)
+					}
 				}
 			}
 		}
